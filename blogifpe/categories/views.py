222de428e4ffb5a7a4f2f861d2dff774f80cc657
Karen Ribeiro <kevr@discente.ifpe.edu.br>
2025-05-18 19:02:50 -0300

    adiciona view em categories

--- blogifpe/categories/views.py
+++ blogifpe/categories/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from categories.models import Category
+from posts.models import Post
+
 
 # Create your views here.
+
+def get_categories(request, name):
+    category = Category.objects.get(name=name)
+    posts_category = Post.objects.filter(category=category).order_by('-created_at')
+    return render (request, 'templates/category_posts.html', {'category': category, 'posts_category': posts_category})
+
