d0e8cdb75e9ff94c49657ef34ddb90f0f02862d6
Karen Ribeiro <kevr@discente.ifpe.edu.br>
2025-05-19 21:33:37 -0300

    adiciona painel de admin para posts e categorias

--- blogifpe/categories/views.py
+++ blogifpe/categories/views.py
@@ -6,7 +6,10 @@ from posts.models import Post
 # Create your views here.
 
 def get_categories(request, name):
-    category = Category.objects.get(name=name)
-    posts_category = Post.objects.filter(category=category).order_by('-created_at')
-    return render (request, 'templates/category_posts.html', {'category': category, 'posts_category': posts_category})
+    try:
+        category = Category.objects.get(name=name)
+        posts_category = Post.objects.filter(category=category).order_by('-created_at')
+        return render (request, 'templates/category_posts.html', {'category': category, 'posts_category': posts_category})
+    except Category.DoesNotExist:
+        return render(request, 'templates/category_posts.html', {'error': 'Category not found'})
 
