ccdaf1e8b5d688cccb6ee65014992bf89d4666a4
Karen Ribeiro <kevr@discente.ifpe.edu.br>
2025-05-20 20:04:22 -0300

    ajusta views de categorias

--- blogifpe/categories/views.py
+++ blogifpe/categories/views.py
@@ -3,7 +3,6 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework import status
 from rest_framework.response import Response
-from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from categories.models import Category
 from posts.models import Post
