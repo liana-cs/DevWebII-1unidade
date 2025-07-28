457b440fcc7f511b274c18b14b020eb61eabd739
Liana Carvalho <lcs44@discente.ifpe.edu.br>
2025-07-27 22:34:18 -0300

    Correção do network docker

--- blogifpe/blog-frontend/src/components/ProtectedRoute.js
+++ blogifpe/blog-frontend/src/components/ProtectedRoute.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
-import { CircularProgress, Box } from '@mui/material';
 
 const ProtectedRoute = ({ children }) => {
   const { user, loading } = useAuth();
